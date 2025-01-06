73d2e899aee90b02afc61b0a6365a1ca00c7fe9b
kngsujng <kngsujng@naver.com>
2025-01-07 00:28:02 +0900

    chore: api url을 environments serverUrl로 대체

--- src/entities/auth/api/auth.api.ts
+++ src/entities/auth/api/auth.api.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from "@angular/common/http";
-import { inject } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
+import { environment } from "src/shared";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthApi {
   private http = inject(HttpClient);
 
   onLogin(loginObj: { username: string, password: string }) {
-    return this.http.post('server_url', loginObj);
+    return this.http.post(`${environment.serverUrl}/api/v1/auth/login`, loginObj);
   }
 }
