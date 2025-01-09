2b707848ae79fa0b6ad93f84e3c0b50e7b48c035
강 수정 <kangsujeong@gmail.com>
2025-01-09 23:01:16 +0900

    refactor: 이메일 중복 검증 비동기 처리

--- src/pages/register/register.page.ts
+++ src/pages/register/register.page.ts
@@ -1,66 +1,55 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
 import { AuthApi, SignUpFormT } from 'src/entities/auth';
 import { UserApi } from 'src/entities/user';
+import { CommonModule } from '@angular/common';
+import { CustomValidators } from 'src/shared/validators';
 
 @Component({
   selector: 'register-page',
   templateUrl: './register.page.html',
-  imports: [ReactiveFormsModule, RouterLink]
+  imports: [ReactiveFormsModule, RouterLink, CommonModule]
 })
 export class RegisterPage implements OnInit {
-  public signUpForm = new FormGroup<SignUpFormT>({
-    email: new FormControl('', { validators: [Validators.required, Validators.email], nonNullable: true }),
-    password: new FormControl('', { validators: [Validators.required], nonNullable: true }),
-  })
-  public isLoading = signal<boolean>(false);
+  public signUpForm!: FormGroup<SignUpFormT>;
   public message = signal<string | null>(null);
 
   private readonly authApi = inject(AuthApi);
   private readonly userApi = inject(UserApi);
   private readonly router = inject(Router);
+  private readonly customValidators = inject(CustomValidators);
 
-  constructor() { }
-
-  ngOnInit(){
-    this.signUpForm.get('email')?.valueChanges
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged()
-      )
-      .subscribe(email => {
+  ngOnInit() {
+    this.signUpForm.get('email')?.statusChanges.subscribe((status) => {
+      if (status === 'INVALID') {
         const emailControl = this.signUpForm.get('email');
 
-        if(emailControl?.invalid){
-          if(emailControl.errors?.['email']){
-            this.message.set('유효한 이메일 형식이 아닙니다.');
-          }
-          return;
+        if (emailControl?.hasError('required')) {
+          this.message.set('이메일은 필수입니다.');
+        } else if (emailControl?.hasError('email')) {
+          this.message.set('유효한 이메일 형식이 아닙니다.');
+        } else if (emailControl?.hasError('duplicateEmail')) {
+          this.message.set('사용 중인 이메일입니다.');
         }
+      } else {
+        this.message.set(null); 
+      }
+    });
 
-        if(email) {
-          this.message.set(null);
-          this.isLoading.set(true);
-  
-          this.userApi.onDuplicateEmail(email).subscribe({
-            next: (res) => {
-              if(res.isDuplicate){
-                this.message.set('사용중인 이메일입니다.');
-              } else {
-                this.message.set(null);
-              }
-              this.isLoading.set(false);
-            }, 
-            error: (err) => {
-              this.message.set(err);
-              this.isLoading.set(false);
-            }
-          });
-        };
-      })
   }
+
+  constructor() {
+    this.signUpForm = new FormGroup<SignUpFormT>({
+      email: new FormControl('', { 
+        validators: [Validators.required, Validators.email], 
+        asyncValidators: [this.customValidators.checkDuplicateEmail((email) => this.userApi.onDuplicateEmail(email))],
+        updateOn: 'change', 
+        nonNullable: true
+      }),
+      password: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+    })
+   }
 
   onRegister(){
     // form.value와 form.getRawValue의 차이점 ! 
