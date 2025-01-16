e90f95a36180cd0cca86ee85b125487412b43784
강 수정 <kngsujng@naver.com>
2025-01-17 00:21:57 +0900

    feat: 내 프로필 조회 api 및 타입 설정

--- src/entities/user/api/user.api.ts
+++ src/entities/user/api/user.api.ts
@@ -1,14 +1,22 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
-import { environment } from "src/shared/environments";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/shared/environments';
+import { ProfileDTO } from '../model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserApi {
   private http = inject(HttpClient);
 
-  onDuplicateEmail(email: string) {
-    return this.http.get<{ isDuplicate: boolean }>(`${environment.serverUrl}/api/v1/users/emails/${email}/duplicated`)
+  // 이메일 중복 검증
+  checkDuplicatedEmail(email: string) {
+    return this.http.get<{ isDuplicate: boolean }>(`${environment.serverUrl}/api/v1/users/emails/${email}/duplicated`);
+  }
+
+  // 내 프로필 조회
+  getProfile(): Observable<ProfileDTO> {
+    return this.http.get<ProfileDTO>(`${environment.serverUrl}/api/v1/users/me`);
   }
 }
---
+++ src/entities/user/model/user.dto.ts
@@ -0,0 +1,6 @@
+export type ProfileDTO = {
+  id: string;
+  nickname: string;
+  email: string;
+  createdAt: string;
+};
