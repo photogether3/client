0731029aac75bed4cb939b1bd01c9bd8e56343e4
강 수정 <kngsujng@naver.com>
2025-01-17 14:22:05 +0900

    feat: 모달 서비스 생성

---
+++ src/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,58 @@
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from "@angular/cdk/portal";
+import { inject, Injectable, Type } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  public data?: any;
+
+  private result$?: Subject<any>;
+  private overlayRef: OverlayRef | null = null;
+  private overlay = inject(Overlay);
+
+
+  open<T>(component: Type<T>, data?: any) {
+    if (this.overlayRef) {
+      this.close();
+    }
+
+    this.data = data;
+    this.result$ = new Subject<any>();
+
+    const overlayConfig = this.overlay.create({
+      hasBackdrop: true,
+      backdropClass: 'cdk-overlay-dark-backdrop',
+      panelClass: 'cdk-overlay-panel',
+      positionStrategy: this.overlay.position()
+        .global()
+        .centerHorizontally()
+        .centerVertically()
+    });
+
+    const portal = new ComponentPortal(component);
+    overlayConfig.attach(portal);
+
+    this.overlayRef = overlayConfig;
+    overlayConfig.backdropClick().subscribe(() => {
+      this.close();
+    })
+
+    return this.result$.asObservable();
+  }
+
+  close(result?: any) {
+    if (this.overlayRef) {
+      this.result$?.next(result);
+      this.result$?.complete();
+
+      this.overlayRef.detach();
+      this.overlayRef.dispose();
+      this.overlayRef = null;
+
+      this.result$ = undefined;
+    }
+  }
+}
