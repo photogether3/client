51ecbb6fdfce79b0ecf791b2e0c146753736f3fd
강 수정 <kngsujng@naver.com>
2025-01-20 23:26:56 +0900

    chore(svg-icon): angular-svg-icon 패키지 설치

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor, errorInterceptor } from '../shared/interceptors';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideAngularSvgIcon(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
