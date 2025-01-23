9b1967117eeac143c595251a6ccc281c16859210
강 수정 <kngsujng@naver.com>
2025-01-23 23:57:26 +0900

    design(footer): footer 위젯 라우터링크 범위 변경

--- src/widgets/footer/footer.widget.ts
+++ src/widgets/footer/footer.widget.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IconComponent } from 'src/shared/components';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.widget.html',
-  imports: [IconComponent],
+  imports: [IconComponent, RouterLink],
 })
 export class FooterWidget {
   constructor() {}
