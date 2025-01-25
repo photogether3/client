61c90c71eef8aaffa1c3769757da3dcaf6c8c110
강 수정 <kngsujng@naver.com>
2025-01-25 23:57:44 +0900

    feat(category): 카테고리 api 및 DTO 설정

---
+++ src/entities/category/api/category.api.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/shared/environments';
+import { CategoriesDTO } from '../model';
+import { skipAuth } from 'src/shared/interceptors';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryApi {
+  private http = inject(HttpClient);
+
+  // 전체 카테고리 목록 조회
+  fetchCategories(): Observable<CategoriesDTO[]> {
+    return this.http.get<CategoriesDTO[]>(`${environment.serverUrl}/v1/categories`, { context: skipAuth() });
+  }
+}
---
+++ src/entities/category/model/category.dto.ts
@@ -0,0 +1,4 @@
+export type CategoriesDTO = {
+  categoryId: string;
+  name: string;
+};
