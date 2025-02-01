42ff684532a0783cc6907ea80b3b5bb48559f4ed
강 수정 <kngsujng@naver.com>
2025-02-01 20:54:00 +0900

    feat(home): 홈 프로필 사용자 프로필 조회 api 호출

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { AlbumApi, AlbumDTO } from 'src/entities/album';
+import { UserApi } from 'src/entities/user';
 import { ButtonComponent, IconComponent } from 'src/shared/components';
 import { FooterWidget } from 'src/widgets/footer';
 import { AlbumCardComponent } from './components';
-import { CommonModule } from '@angular/common';
-import { AlbumApi, AlbumDTO } from 'src/entities/album';
 
 @Component({
   selector: 'home-page',
@@ -12,11 +12,17 @@ import { AlbumApi, AlbumDTO } from 'src/entities/album';
   imports: [FooterWidget, IconComponent, AlbumCardComponent, ButtonComponent, CommonModule],
 })
 export class HomePage {
-  private router = inject(Router);
-  private albumApi = inject(AlbumApi);
+  public nickname: string = '';
   public albumList: AlbumDTO[] | undefined = undefined;
 
+  private readonly userApi = inject(UserApi);
+  private readonly albumApi = inject(AlbumApi);
+
   constructor() {
+    this.userApi.getProfile().subscribe((res) => {
+      this.nickname = res.nickname;
+    });
+
     this.albumApi.getCollections().subscribe((res) => {
       this.albumList = res;
     });
