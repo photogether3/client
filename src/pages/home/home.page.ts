dca6bfd31262f5c93a0053f190a35e3cc16ca52f
강 수정 <kngsujng@naver.com>
2025-02-21 22:23:28 +0900

    feat(home): 홈페이지 api 병렬 호출 및 사진첩 타입 분류

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,35 +1,61 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CollectionApi, CollectionsResDTO, CollectionType } from 'src/entities/collection';
+import { filter, forkJoin, switchMap, tap } from 'rxjs';
+import { CategoryApi } from 'src/entities/category';
+import { CollectionApi, CollectionType } from 'src/entities/collection';
 import { UserApi } from 'src/entities/user';
 import { ButtonComponent, IconComponent } from 'src/shared/components';
 import { FooterWidget } from 'src/widgets/footer';
-import { CollectionCardComponent } from './components';
 import { HeaderWidget } from 'src/widgets/header';
+import { CollectionCardComponent } from './components';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.page.html',
   imports: [FooterWidget, IconComponent, CollectionCardComponent, ButtonComponent, CommonModule, HeaderWidget],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   public nickname: string = '';
-  // public collectionList: CollectionDTO[] | undefined = undefined;
-  public collectionList: CollectionType[] | undefined = undefined;
+  public collectionList: CollectionType[] = [];
 
   private readonly router = inject(Router);
   private readonly userApi = inject(UserApi);
+  private readonly categoryApi = inject(CategoryApi);
   private readonly collectionApi = inject(CollectionApi);
 
-  constructor() {
-    this.userApi.getProfile().subscribe((res) => {
-      this.nickname = res.nickname;
-    });
+  get myCollections() {
+    return {
+      default: this.collectionList.filter((collection) => collection.type === 'DEFAULT'),
+      uncategorized: this.collectionList.find((collection) => collection.type === 'UNCATEGORIZED'),
+      trash: this.collectionList.find((collection) => collection.type === 'TRASH'),
+    };
+  }
 
-    this.collectionApi.getCollections().subscribe((res: CollectionsResDTO) => {
-      this.collectionList = res.items;
-    });
+  constructor() {}
+
+  ngOnInit(): void {
+    this.categoryApi
+      .fetchFavCategories()
+      .pipe(
+        tap((res) => {
+          console.log(res);
+          if (res.length === 0) {
+            this.router.navigateByUrl('/onboarding');
+          }
+        }),
+        filter((res) => res.length > 0),
+        switchMap(() =>
+          forkJoin({
+            profile: this.userApi.getProfile(),
+            collections: this.collectionApi.getCollections(),
+          }),
+        ),
+      )
+      .subscribe(({ profile, collections }) => {
+        this.nickname = profile.nickname;
+        this.collectionList = collections.items;
+      });
   }
 
   // 사진첩 생성
