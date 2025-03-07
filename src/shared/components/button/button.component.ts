996960c3af3235380279cd386a9663618924c428
강 수정 <kngsujng@naver.com>
2025-03-08 00:12:02 +0900

    feat(button): 버튼 컴포넌트 아이콘 추가

--- src/shared/components/button/button.component.ts
+++ src/shared/components/button/button.component.ts
@@ -1,14 +1,28 @@
 import { Component, computed, input } from '@angular/core';
+
+import { IconComponent } from '../icon';
+
 import { ButtonProps, buttonStyles, customTwMerge } from './button.styles';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
+  styles: `
+    :host {
+      display: flex;
+      justify-center: center;
+      align-content: center;
+    }
+  `,
+  imports: [IconComponent],
 })
 export class ButtonComponent {
   text = input.required();
   type = input<'button' | 'submit'>('button');
   isDisabled = input<boolean>(false);
+  leftIcon = input<string>();
+  rightIcon = input<string>();
+  iconSize = input<number>();
   appearance = input<ButtonProps['type']>();
   size = input<ButtonProps['size']>();
   width = input<ButtonProps['width']>();
