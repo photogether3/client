8f1a3c4fc865ccc3c82823f9f627055fb7fcf174
강 수정 <kngsujng@naver.com>
2025-03-09 21:11:38 +0900

    design(modal): common-modal 컴포넌트 디자인 반영

--- src/shared/components/modal/ui/common-modal.component.ts
+++ src/shared/components/modal/ui/common-modal.component.ts
@@ -1,17 +1,19 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
+
 import { ButtonComponent } from '../../button';
 import { ReactiveModalData } from '../services';
+import { IconComponent } from '../../icon';
 
 @Component({
   selector: 'app-common-modal',
   templateUrl: 'common-modal.component.html',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, IconComponent],
 })
 export class CommonModalComponent {
-  public data = inject<ReactiveModalData>(DIALOG_DATA);
-
   private readonly dialogRef = inject(DialogRef);
+
+  data = inject<ReactiveModalData>(DIALOG_DATA);
 
   constructor() {}
 
