6cf1ec4b94cba1364ed8b29111cfa6f7cf54b7cb
kngsujng <kngsujng@naver.com>
2025-03-17 21:01:35 +0900

    fix(otp): otp 인증 api 호출 성공여부에 따라 타이머 동작 컨트롤

--- src/pages/otp-verify/ui/page/otp-verify.page.ts
+++ src/pages/otp-verify/ui/page/otp-verify.page.ts
@@ -1,22 +1,33 @@
-import { Component, inject } from '@angular/core';
-import { AuthApi } from 'src/entities/auth';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { ButtonComponent } from 'src/shared/components';
+
+import { AuthApi } from 'src/entities/auth';
+
 import { OtpVerifyFormComponent } from '../form';
 
 @Component({
   selector: 'otp-verify-page',
   templateUrl: './otp-verify.page.html',
-  imports: [OtpVerifyFormComponent, ButtonComponent],
+  imports: [OtpVerifyFormComponent],
 })
 export class OtpVerifyPage {
   private readonly authApi = inject(AuthApi);
   private readonly router = inject(Router);
 
+  otpSent = signal<boolean>(false);
+
   constructor() {
     const navigation = this.router.getCurrentNavigation();
     const email = navigation?.extras?.state?.['email'] || null;
 
-    // this.authApi.generateOtp({ email }).subscribe();
+    this.authApi.generateOtp({ email }).subscribe({
+      next: () => {
+        this.otpSent.set(true);
+      },
+      error: (err) => {
+        console.error('otp 요청 실패', err);
+        this.otpSent.set(false);
+      },
+    });
   }
 }
