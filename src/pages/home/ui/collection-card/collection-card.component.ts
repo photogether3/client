08495b6ef63a9aa028715c33a5f14193c521ee78
강 수정 <kngsujng@naver.com>
2025-03-20 20:46:10 +0900

    rename: collection-card 컴포넌트 input변수명 post->collection으로 변경

--- src/pages/home/ui/collection-card/collection-card.component.ts
+++ src/pages/home/ui/collection-card/collection-card.component.ts
@@ -5,30 +5,40 @@ import { Router } from '@angular/router';
 import { TagComponent } from 'src/entities/category';
 import { CollectionType } from 'src/entities/collection';
 import { IconComponent } from 'src/shared/components';
+import { ClickOutsideDirective } from 'src/shared/directives';
+
+import { PopoverComponent } from '../popover';
 
 @Component({
   selector: 'app-collection-card',
   templateUrl: './collection-card.component.html',
-  imports: [CommonModule, TagComponent, IconComponent],
+  imports: [CommonModule, TagComponent, IconComponent, PopoverComponent, ClickOutsideDirective],
 })
 export class CollectionCardComponent {
   private readonly router = inject(Router);
 
-  post = input.required<CollectionType>();
+  collection = input.required<CollectionType>();
   isOpenPopover = signal<boolean>(false);
 
   constructor() {}
 
-  handlePopover() {
+  handlePopover(event: Event) {
+    event.stopPropagation();
     this.isOpenPopover.update((prev) => !prev);
   }
 
+  closePopover() {
+    this.isOpenPopover.set(false);
+  }
+
   goPage() {
-    // const url = this.router.url;
-    // if (url.includes('home')) {
-    //   this.router.navigateByUrl('collection/' + this.post().id);
-    // } else {
-    //   return;
-    // }
+    if (this.isOpenPopover()) return;
+
+    const url = this.router.url;
+    if (url.includes('home')) {
+      this.router.navigateByUrl('collection/' + this.collection().id);
+    } else {
+      return;
+    }
   }
 }
