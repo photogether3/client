385e592630d2eeb88ed575e091253225894ef859
강 수정 <kngsujng@naver.com>
2025-03-31 20:02:05 +0900

    design(tag): 태그 컴포넌트 디자인 업데이트

--- src/entities/category/ui/tag/tag.component.ts
+++ src/entities/category/ui/tag/tag.component.ts
@@ -1,8 +1,7 @@
 import { Component, computed, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TagProps, tagVariants } from './tag.styles';
-import { customTwMerge } from 'src/shared/components/button/button.styles';
+import { customTwMerge, TagProps, tagVariants } from './tag.styles';
 
 @Component({
   selector: 'app-tag',
@@ -23,5 +22,4 @@ export class TagComponent {
     return customTwMerge(tagVariants({ type: this.type(), size: this.size() }));
   });
 }
-
 // TODO customTwMerge shared로 분리해야 함
