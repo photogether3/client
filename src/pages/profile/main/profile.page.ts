30afab100807fc26b9e3d94c2c385cc5b225e6b2
강 수정 <kngsujng@naver.com>
2025-04-05 00:40:01 +0900

    design(post): 프로필 페이지 디자인 업데이트

--- src/pages/profile/main/profile.page.ts
+++ src/pages/profile/main/profile.page.ts
@@ -1,27 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Type } from '@angular/core';
 import { Router } from '@angular/router';
-import { filter, forkJoin, map, switchMap } from 'rxjs';
+
+import { forkJoin } from 'rxjs';
+
 import { AuthApi, AuthService } from 'src/entities/auth';
 import { CategoriesGetDTO, CategoryApi, TagComponent } from 'src/entities/category';
 import { ProfileGetDTO, UserApi } from 'src/entities/user';
-import { ButtonComponent, ModalReactiveService, ModalService } from 'src/shared/components';
+import { BottomSheetService, IconComponent } from 'src/shared/components';
+import { ActionButtonsComponent, ActionButtonType } from 'src/widgets/action-buttons';
 import { FooterWidget } from 'src/widgets/footer';
 import { HeaderWidget } from 'src/widgets/header';
 
 @Component({
   selector: 'profile-page',
   templateUrl: './profile.page.html',
-  imports: [TagComponent, ButtonComponent, FooterWidget, HeaderWidget],
+  imports: [TagComponent, FooterWidget, HeaderWidget, IconComponent],
 })
 export class ProfilePage {
-  public profile: (ProfileGetDTO & { tags: CategoriesGetDTO[] }) | undefined = undefined;
-
   private readonly router = inject(Router);
-  private readonly modalService = inject(ModalService);
-  private readonly modalReactiveService = inject(ModalReactiveService);
+  private readonly bottomSheetService = inject(BottomSheetService);
   private readonly authApi = inject(AuthApi);
   private readonly userApi = inject(UserApi);
   private readonly categoryApi = inject(CategoryApi);
+
+  profile: (ProfileGetDTO & { tags: CategoriesGetDTO[] }) | undefined = undefined;
 
   constructor() {
     forkJoin({
@@ -35,7 +37,51 @@ export class ProfilePage {
     });
   }
 
-  // 비밀번호 변경
+  onLogout() {
+    this.authApi.logout().subscribe(() => {
+      const instance = AuthService.getInstance();
+      instance.clear();
+      this.router.navigateByUrl('login');
+    });
+  }
+
+  async openBottomSheet() {
+    const actionButtons: ActionButtonType[] = [
+      {
+        type: 'update',
+        icon: 'post',
+        text: '프로필 편집',
+        appearance: 'primary',
+      },
+      {
+        type: 'privacy',
+        icon: 'user',
+        text: '개인정보 관리',
+        appearance: 'secondary',
+      },
+      {
+        type: 'logout',
+        icon: 'logout',
+        text: '로그아웃',
+        appearance: 'danger',
+      },
+    ];
+
+    const result = await this.bottomSheetService.open(ActionButtonsComponent as Type<Component>, actionButtons);
+
+    switch (result) {
+      case 'update':
+        return this.router.navigateByUrl('/profile/update');
+      case 'privacy':
+        // TODO 개인정보 관리 모달 띄우기
+        return console.log('adf');
+      case 'logout':
+        return this.onLogout();
+    }
+
+    this.bottomSheetService.open(ActionButtonsComponent as Type<Component>);
+  }
+
   // TODO 비밀번호 변경 실패했을 때 보여줘야 함
   updatePassword() {
     // this.modalService
@@ -58,26 +104,12 @@ export class ProfilePage {
     //   });
   }
 
-  // 로그아웃
-  onLogout() {
-    this.authApi.logout().subscribe(() => {
-      const instance = AuthService.getInstance();
-      instance.clear();
-      this.router.navigateByUrl('login');
-    });
-  }
-
-  // 프로필 수정 페이지 이동
-  goUpdatePage() {
-    this.router.navigateByUrl('/profile/update');
-  }
-
-  // 기록 초기화
+  // TODO 기록초기화
   reset() {
     alert('기능 개발중 ..');
   }
 
-  // 회원탈퇴
+  // TODO 회원탈퇴
   withdraw() {
     alert('기능 개발중 ...');
   }
