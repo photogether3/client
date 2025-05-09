ec9fa204c4bda729bbb83ba71da7a0a05ef105ae
강 수정 <kngsujng@naver.com>
2025-05-10 01:30:46 +0900

    feat(popover): 팝오버 라우터 변경시에도 안 닫히는 문제 해결

--- src/shared/directives/popover.directive.ts
+++ src/shared/directives/popover.directive.ts
@@ -1,6 +1,10 @@
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { Directive, HostListener, inject, input, model, signal } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
+import { filter } from 'rxjs';
+
 import { PopoverComponent, PopoverItemType } from 'src/pages/home';
 
 @Directive({
@@ -8,6 +12,7 @@ import { PopoverComponent, PopoverItemType } from 'src/pages/home';
 })
 export class PopoverDirective {
   private readonly overlay = inject(Overlay);
+  private readonly router = inject(Router);
 
   button = input.required<HTMLElement>();
   popoverItems = input.required<PopoverItemType[]>();
@@ -15,6 +20,14 @@ export class PopoverDirective {
   isPopoverOpen = model<boolean>();
 
   private overlayRef = signal<OverlayRef | null>(null);
+
+  constructor() {
+    this.router.events.pipe(filter((event) => event instanceof NavigationStart)).subscribe(() => {
+      if (this.overlayRef()) {
+        this.closePopover();
+      }
+    });
+  }
 
   @HostListener('click', ['$event'])
   togglePopover(event: MouseEvent) {
